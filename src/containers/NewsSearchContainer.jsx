5963e10d9f4909f866d98e5cb73de5353ad3385e
Justin W Soto <soto@pdx.edu>
2021-11-03 15:14:31 -0700

    added api key and news search container component

---
+++ src/containers/NewsSearchContainer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+
+class NewsSearchContainer extends Component {
+    render() { 
+        return <h1>Loading...</h1>;
+    }
+}
+ 
+export default NewsSearchContainer;
