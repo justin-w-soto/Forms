248ad8db26a55e20510349469d08c3f5ecf71062
Justin W Soto <soto@pdx.edu>
2021-11-04 15:34:55 -0700

    added style to form and button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 
 export default function Search({ onSubmit, query, onChange }) {
     return(
-        <form onSubmit={onSubmit}>
-            <label htmlFor="articleName">Search Articles</label>
+        <div style={{backgroundColor: '#85756E', color: '#CDC5B4', height: '200', padding: '1em'}}>
+        <form onSubmit={onSubmit} >
+            <label htmlFor="articleName" style={{color:'#DED9CE', fontSize: '2em', fontFamily: 'monospace', fontWeight: '900', margin: '1em'}}> SEARCH ARTICLES</label>
 
             <input 
                 id="articleName"
@@ -12,9 +13,11 @@ export default function Search({ onSubmit, query, onChange }) {
                 type="text"
                 value={query}
                 onChange={onChange} 
+                style={{marginInline:'.5em', color: '#551B14', fontSize: '1em', fontWeight: '900', fontFamily: 'monospace'}}
             />
-                  <button aria-label="news"> Search </button>
+                  <button aria-label="search-articles"  style={{backgroundColor: '#6D3D14', color: 'whitesmoke', fontSize:'1em', fontWeight: '500', borderRadius: '.35em', fontFamily: 'monospace' }}> Search </button>
         </form>
+        </div>
     )
 }
 
--- src/services/NewsApi.js
+++ src/services/NewsApi.js
@@ -1,6 +1,6 @@
 export const fetchArticles = async () => {
 
-const res = await fetch('https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.NewsApi_key}');
+const res = await fetch('https://newsapi.org/v2/everything?q=${query}&apiKey=a03d2fb2d29047c19b981bcefb391a60');
 const json = await res.json();
 
   return json.articles.map((article) => ({
@@ -9,5 +9,4 @@ const json = await res.json();
     description: article.description ? article.description : '',
     image: article.urlToImage ? article.urlToImage : 'nope'
   }));
-  
 };
