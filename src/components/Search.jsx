fd1b9c44d7fdf1e2cc0488a32bbadf9c15fd71ae
Justin W Soto <soto@pdx.edu>
2021-11-04 16:09:10 -0700

    styled

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,10 @@ export default function Search({ onSubmit, query, onChange }) {
     return(
         <div style={{backgroundColor: '#85756E', color: '#CDC5B4', height: '200', padding: '1em'}}>
         <form onSubmit={onSubmit} >
-            <label htmlFor="articleName" style={{color:'#DED9CE', fontSize: '2em', fontFamily: 'monospace', fontWeight: '900', margin: '1em'}}> SEARCH ARTICLES</label>
+
+            <label htmlFor="articleName" style={{color:'#DED9CE', fontSize: '2em', 
+            fontFamily: 'times', fontWeight: '900', margin: '1em'}}> SEARCH ARTICLES</label>
+            <br/>
 
             <input 
                 id="articleName"
@@ -13,9 +16,19 @@ export default function Search({ onSubmit, query, onChange }) {
                 type="text"
                 value={query}
                 onChange={onChange} 
-                style={{marginInline:'.5em', color: '#551B14', fontSize: '1em', fontWeight: '900', fontFamily: 'monospace'}}
+                style={{marginLeft:'2em', marginTop: '.25em', color: '#551B14', fontSize: '1em', fontWeight: '900', fontFamily: 'times'}}
             />
-                  <button aria-label="search-articles"  style={{backgroundColor: '#84291F', color: 'whitesmoke', fontSize:'1em', fontWeight: '500', borderRadius: '.35em', fontFamily: 'monospace' }}> Search </button>
+            
+            <button aria-label="search-articles"  
+                style={{backgroundColor: '#84291F', color: 'whitesmoke', fontSize:'1.5em', fontWeight: '500', borderRadius: '.35em', fontFamily: 'times', marginLeft: '.5em' }}>
+                Search
+            </button>
+
+            <nav style= {{fontSize: '8em', display: 'flex', flexDirection: 'row-reverse', marginRight: '2rem',
+                color:'#DED9CE', fontFamily: 'times', fontWeight: '900' }}>
+                BAD NEWS DAILY
+            </nav>
+
         </form>
         </div>
     )
